98e752a3d18e9d92664bf6fae5d5aff66da01a4f
Jordan Cristiano <jordan.cristi@gmail.com>
2015-02-19 21:17:44 -0500

    Added windows support for SDL2 premake scripts

--- premake_scripts/sdl2.lua
+++ premake_scripts/sdl2.lua
@@ -6,15 +6,35 @@ project "_SDL2"
     kind "SharedLib"
     files (base_dir.."/dummy.cpp")
 
+    prebuildcommands ("{TOUCH} "..base_dir.."/dummy.cpp")
     filter "system:not windows"
         prebuildcommands
         {
-            "{TOUCH} "..base_dir.."/dummy.cpp",
             "{CHDIR} "..base_dir.."; [ -f Makefile ] || ./configure; make -j16"
         }
         postbuildcommands
         {
             "{COPY} "..base_dir.."/build/.libs/libSDL2*%{cfg.buildtarget.extension}* "..output_dir
+        }
+    filter { "system:windows", "platforms:x32" }
+        local msbuild = '"'..os.getenv("VS120COMNTOOLS")..'../../../MSBuild/12.0/Bin/MSBuild" "'
+        local build_command = path.translate(msbuild..base_dir..'/VisualC/SDL/SDL_VS2013.vcxproj"')
+        prebuildcommands
+        {
+            build_command.." /p:Configuration=Release /p:Platform=Win32"
+        }
+        postbuildcommands
+        {
+            "{COPY} "..base_dir.."/VisualC/SDL/Win32/Release/SDL2.* "..output_dir
+        }
+    filter { "system:windows", "platforms:x64" }
+        prebuildcommands
+        {
+            build_command.." /p:Configuration=Release /p:Platform=x64"
+        }
+        postbuildcommands
+        {
+            "{COPY} "..base_dir.."/VisualC/SDL/x64/Release/SDL2.* "..output_dir
         }
     filter {}
 project "*"
--- premake_scripts/sdl2_include.lua
+++ premake_scripts/sdl2_include.lua
@@ -9,3 +9,14 @@ links
 {
     "_SDL2"
 }
+filter "system:not windows"
+    postbuildcommands
+    {
+        "{COPY} libSDL2* ../../../bin/"
+    }
+filter "system:windows"
+    postbuildcommands
+    {
+        "{COPY} SDL2.dll ../../../bin/"
+    }
+filter{}
